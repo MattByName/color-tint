5e2ffb476b4120f2b6be0431292f95995d147084
Matt Barnard <matt@serverus.co.uk>
2019-04-27 15:18:57 +0100

    menu now displaying

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 
 const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
@@ -8,13 +9,33 @@ const PopupMenu = imports.ui.popupMenu;
 const PanelMenu = imports.ui.panelMenu;
 
 let tinter = null;
+let menu = null;
 
 const MenuButton = new Lang.Class({
     Name: "MenuButton",
     Extends: PanelMenu.Button,
-    _init: function () {},
-    createButton: function () {},
-    deleteButton: function () {},
+    // Constructor
+    _init: function () {
+        this.parent(1, 'ColorTintMenu', false);
+        let box = new St.BoxLayout();
+        let icon =  new St.Icon({ icon_name: 'system-search-symbolic', style_class: 'system-status-icon'});
+        let toplabel = new St.Label({ text: ' Menu ',
+            y_expand: true,
+            y_align: Clutter.ActorAlign.CENTER });
+
+        // We add the icon, the label and a arrow icon to the box
+        box.add(icon);
+        box.add(toplabel);
+        box.add(PopupMenu.arrowIcon(St.Side.BOTTOM));
+
+        // We add the box to the button
+        // It will be showed in the Top Panel
+        this.actor.add_child(box);
+
+
+    },
+
+
 
 })
 
@@ -41,7 +62,7 @@ const ColorTinter = new Lang.Class({
 
     // disable
     disable : function() {},
-}
+})
 
 function init() {
 
@@ -50,11 +71,14 @@ function init() {
 function enable() {
     tinter = new ColorTinter();
     tinter.enable();
+    menu = new MenuButton();
+    Main.panel.addToStatusArea("ChatStatus", menu, 0, "right");
 
 }
 
 function disable() {
     tinter.disable();
     tinter = null;
+    menu = null;
 
 }
