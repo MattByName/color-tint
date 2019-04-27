d8514b2e2b4a5ce3aa0b44fd6631cf977dfe49d8
Matt Barnard <matt@serverus.co.uk>
2019-04-27 14:47:39 +0100

    added enable and disable skeleton functions

--- extension.js
+++ extension.js
@@ -3,51 +3,47 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
-let text, button;
+const ColorTinter = new Lang.Class({
+    Name: "ColorTinter",
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+    // Create Tint Overlay
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
+    // Delete Tint Overlay
 
-    text.opacity = 255;
+    // Set color of Overlay
 
-    let monitor = Main.layoutManager.primaryMonitor;
+    // Hide Overlay
 
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
+    // Show Overlay
 
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+    // Load Color
+
+    // Save Color
+
+    // Create Menu
+
+    // Delete Menu
+
+    // enable
+    enable: function()
+    {},
+
+    // disable
+    disable : function() {},
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    tinter = new ColorTinter();
+    tinter.enable();
+
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    tinter.disable();
+    tinter = null;
+
 }
