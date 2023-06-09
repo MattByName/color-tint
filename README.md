397ab9ca8f034b72801ddcdc9319018d68d41b60
Matt Barnard <contact@missingbit.co.uk>
2023-06-09 21:08:27 +0100

    cleaned up code based on review

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # ColorTint
+
 ![ColorTint Banner](assets/github_social_card.png)
+
 ## About
+
 ColorTint is a Gnome extension that draws a translucent coloured overlay over your desktop. This is to help people with dyslexia, visual stress, scopic sensitivity, and related conditions.
 
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/E1E1CFXTK)
 
 ### Links
-* [GitHub Repository](https://github.com/MattByName/color-tint)
-* [Gnome Extensions Page](https://extensions.gnome.org/extension/1789/colortint/)
-* [GitHub Releases](https://github.com/MattByName/color-tint/releases)
+
+- [GitHub Repository](https://github.com/MattByName/color-tint)
+- [Gnome Extensions Page](https://extensions.gnome.org/extension/1789/colortint/)
+- [GitHub Releases](https://github.com/MattByName/color-tint/releases)
 
 ## Installation
+
 The easiest way to install is using the Gnome Extensions website. You can find ColorTint [here](https://extensions.gnome.org/extension/1789/colortint/)
 
-You can also install ColorTint manually by downloading the [latest release](https://github.com/MattByName/color-tint/releases) and unzipping it into the Gnome extensions folder on your desktop. You can find a guide on how to do that [here](https://www.ubuntubuzz.com/2017/11/how-to-install-manually-gnome-shell-extension.html). You can also clone this repository and use the terminal command `make install` from the repo root directory. 
-
+You can also install ColorTint manually by downloading the [latest release](https://github.com/MattByName/color-tint/releases) and unzipping it into the Gnome extensions folder on your desktop. You can find a guide on how to do that [here](https://www.ubuntubuzz.com/2017/11/how-to-install-manually-gnome-shell-extension.html). You can also clone this repository and use the terminal command `make install` from the repo root directory.
 
 ## Support for GNOME versions
-I attempt to maintain backwards compatibility, but if an upgrade breaks support for a release that has reached end of 
+
+I attempt to maintain backwards compatibility, but if an upgrade breaks support for a release that has reached end of
 life, I will update the table below to show which version of ColorTint was last verified to work with it.
 
 | GNOME Version | ColorTint Version                                                      | GNOME Extensions Site Version |
-|:--------------|:-----------------------------------------------------------------------|:------------------------------|
+| :------------ | :--------------------------------------------------------------------- | :---------------------------- |
+| 44            | [latest](https://github.com/MattByName/color-tint/releases/latest)     |                               |
 | 43            | [latest](https://github.com/MattByName/color-tint/releases/latest)     |                               |
 | 42            | [latest](https://github.com/MattByName/color-tint/releases/latest)     |                               |
 | 41            | [v2.2.2](https://github.com/MattByName/color-tint/releases/tag/v2.2.2) | 14                            |
@@ -32,16 +38,17 @@ life, I will update the table below to show which version of ColorTint was last 
 | 3.32          | [v1.1.1](https://github.com/MattByName/color-tint/releases/tag/v1.1.1) | 4                             |
 
 ## Features
-* Set any colour using RGB sliders
-* Custom translucency through alpha slider
-* ColorTint remembers your chosen color, automatically saving it when you change it, and loading on launch
-* The tint is on by default. Click the colortint icon and toggle "Tint" to enable. Go to settings in the Gnome Extensions app to change the default behaviour
-* Supports multiple monitor setup
+
+- Set any colour using RGB sliders
+- Custom translucency through alpha slider
+- ColorTint remembers your chosen color, automatically saving it when you change it, and loading on launch
+- The tint is on by default. Click the colortint icon and toggle "Tint" to enable. Go to settings in the Gnome Extensions app to change the default behaviour
+- Supports multiple monitor setup
 
 ## Contributing
+
 Please do contribute if you want to change something or help out with one of the issues.
 
 ## Support, Feature Requests and Bug Reports
 
 Please [raise an issue](https://github.com/MattByName/color-tint/issues/new) or raise a pull request. Alternatively, you can contact me via matrix at [@mattbyname:matrix.org](https://matrix.to/#/@mattbyname:matrix.org)
-
--- src/extension.js
+++ src/extension.js
@@ -11,270 +11,280 @@ let menu = null;
 let overlay = null;
 
 let overlay_color = {
-    red: 20,
-    green: 20,
-    blue: 20,
-    alpha: 80,
-
+  red: 20,
+  green: 20,
+  blue: 20,
+  alpha: 80,
 };
 
 let overlay_active = false;
 const ExtensionSystem = imports.ui.extensionSystem;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const ShellVersion = imports.misc.config.PACKAGE_VERSION.split('.');
+const ShellVersion = imports.misc.config.PACKAGE_VERSION.split(".");
 
-let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.colortint');
 let ExtensionPath;
-if (ShellVersion[1] === 2)
-    ExtensionPath = ExtensionSystem.extensionMeta['colortint@matt.serverus.co.uk'].path;
-else
-    ExtensionPath = imports.misc.extensionUtils.getCurrentExtension().path;
-
-
-const ColorTinter = GObject.registerClass({
-    GTypeName: 'ColorTinter',
-}, class ColorTinter extends GObject.Object {
+else ExtensionPath = imports.misc.extensionUtils.getCurrentExtension().path;
+ExtensionPath = Me.path;
+const ColorTinter = GObject.registerClass(
+  {
+    GTypeName: "ColorTinter",
+  },
+  class ColorTinter extends GObject.Object {
     // Create Tint Overlay
 
     createOverlay() {
-        /*
+      /*
         Set the overlay to 100x the primary monitor's width and height. Set the overlay x and y to 0.
         This is hacky, but should cover most multi-setups.
         What should be done, is to iterate over all monitors, and create a seperate overlay for each that fills each
         according to its dimensions. If anyone wants to refactor this in that way, please do.
          */
 
-        let monitor = Main.layoutManager.primaryMonitor;
-        overlay = new St.Bin({reactive: false});
-        overlay.set_size(monitor.width * 100, monitor.height * 100);
-        overlay.opacity = 255;
-        overlay.set_position(0, 0);
-        // Arbitrary z position above everything else
-        overlay.set_z_position(650);
+      let monitor = Main.layoutManager.primaryMonitor;
+      overlay = new St.Bin({ reactive: false });
+      overlay.set_size(monitor.width * 100, monitor.height * 100);
+      overlay.opacity = 255;
+      overlay.set_position(0, 0);
+      // Arbitrary z position above everything else
+      overlay.set_z_position(650);
 
-        this.setOverlayColor();
+      this.setOverlayColor();
     }
-
-
 
     // Update color of Overlay
     setOverlayColor() {
-        var color = new Clutter.Color(
-            {
-                red: overlay_color['red'],
-                green: overlay_color['green'],
-                blue: overlay_color['blue'],
-                alpha: overlay_color['alpha'],
-            });
-        overlay.set_background_color(color);
-        this.saveColor();
+      var color = new Clutter.Color({
+        red: overlay_color["red"],
+        green: overlay_color["green"],
+        blue: overlay_color["blue"],
+        alpha: overlay_color["alpha"],
+      });
+      overlay.set_background_color(color);
+      this.saveColor();
     }
-
 
     // Hide Overlay
     hide() {
-        overlay_active = false;
-        Main.uiGroup.remove_actor(overlay);
+      overlay_active = false;
+      Main.uiGroup.remove_actor(overlay);
     }
-
 
     // Show Overlay
     show() {
-        Main.uiGroup.add_actor(overlay);
-        overlay_active = true;
+      Main.uiGroup.add_actor(overlay);
+      overlay_active = true;
     }
 
     // Load Color
     loadColor() {
-        // Load last from json
+      // Load last from json
 
-        this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
-        if (this._file.query_exists(null)) {
-            var flag;
-            var data;
-            [flag, data] = this._file.load_contents(null);
+      this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
+      if (this._file.query_exists(null)) {
+        var flag;
+        var data;
+        [flag, data] = this._file.load_contents(null);
 
-            if (flag) {
-                const ByteArray = imports.byteArray;
-                let prepData = data instanceof Uint8Array ? ByteArray.toString(data) : data.toString();
-                overlay_color = JSON.parse(prepData);
-            }
+        if (flag) {
+          const ByteArray = imports.byteArray;
+          let prepData =
+            data instanceof Uint8Array
+              ? ByteArray.toString(data)
+              : data.toString();
+          overlay_color = JSON.parse(prepData);
         }
+      }
     }
-
-
 
     // Save Color
     saveColor() {
-        this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
-        this._file.replace_contents(JSON.stringify(overlay_color), null, false, 0, null);
+      this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
+      this._file.replace_contents(
+        JSON.stringify(overlay_color),
+        null,
+        false,
+        0,
+        null
+      );
     }
-
 
     // enable
     start_up() {
-        overlay_active = false;
-        this.loadColor();
-        this.createOverlay();
-        if (settings.get_boolean('autostart'))
-            this.show();
+      overlay_active = false;
+      this.loadColor();
+      this.createOverlay();
+      if (settings.get_boolean("autostart")) this.show();
+      let settings = ExtensionUtils.getSettings(
+        "org.gnome.shell.extensions.colortint"
+      );
     }
-
-
 
     // disable
     stop_now() {
-        if (overlay_active == true)
-            Main.uiGroup.remove_actor(overlay);
+      if (overlay_active == true) Main.uiGroup.remove_actor(overlay);
 
-        overlay.destroy();
-        overlay = null;
+      overlay.destroy();
+      overlay = null;
+
+      settings = null;
     }
-});
+  }
+);
 
-const MenuButton = GObject.registerClass({GTypeName: 'MenuButton'}, class MenuButton extends PanelMenu.Button {
+const MenuButton = GObject.registerClass(
+  { GTypeName: "MenuButton" },
+  class MenuButton extends PanelMenu.Button {
     // Constructor
     _init() {
-        super._init(1, 'ColorTintMenu', false);
-        let box = new St.BoxLayout();
-        let icon = new St.Icon({icon_name: 'applications-graphics-symbolic', style_class: 'system-status-icon'});
+      super._init(1, "ColorTintMenu", false);
+      let box = new St.BoxLayout();
+      let icon = new St.Icon({
+        icon_name: "applications-graphics-symbolic",
+        style_class: "system-status-icon",
+      });
 
-        // We add the icon
-        let iconName = '';
-        if (settings.get_boolean('monochrome-icon'))
-            iconName = 'icon_mono.svg';
+      // We add the icon
+      let iconName = "";
+      if (settings.get_boolean("monochrome-icon")) iconName = "icon_mono.svg";
+      else iconName = "icon.svg";
 
-        else
-            iconName = 'icon.svg';
+      icon.gicon = Gio.icon_new_for_string(`${Me.path}/${iconName}`);
+      icon.set_icon_size(20);
+      box.add(icon);
 
-        icon.gicon = Gio.icon_new_for_string(`${Me.path}/${iconName}`);
-        icon.set_icon_size(20);
-        box.add(icon);
+      // We add the box to the button
+      // It will be showed in the Top Panel
+      this.add_child(box);
 
-        // We add the box to the button
-        // It will be showed in the Top Panel
-        this.add_child(box);
+      let popupMenuExpander = new PopupMenu.PopupSubMenuMenuItem(
+        "PopupSubMenuMenuItem"
+      );
 
-        let popupMenuExpander = new PopupMenu.PopupSubMenuMenuItem('PopupSubMenuMenuItem');
+      // This is an example of PopupMenuItem, a menu item. We will use this to add as a submenu
+      let submenu = new PopupMenu.PopupMenuItem("PopupMenuItem");
 
-        // This is an example of PopupMenuItem, a menu item. We will use this to add as a submenu
-        let submenu = new PopupMenu.PopupMenuItem('PopupMenuItem');
+      // A new label
+      let label = new St.Label({ text: "Item 1" });
 
-        // A new label
-        let label = new St.Label({text: 'Item 1'});
+      // Add the label and submenu to the menu expander
+      popupMenuExpander.menu.addMenuItem(submenu);
+      popupMenuExpander.menu.box.add(label);
 
-        // Add the label and submenu to the menu expander
-        popupMenuExpander.menu.addMenuItem(submenu);
-        popupMenuExpander.menu.box.add(label);
+      // Other standard menu items
+      let offswitch = new PopupMenu.PopupSwitchMenuItem("Tint", overlay_active);
 
-        // Other standard menu items
-        let offswitch = new PopupMenu.PopupSwitchMenuItem('Tint', overlay_active);
+      // Assemble all menu items
 
+      // This is a menu separator
+      this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+      this.menu.addMenuItem(offswitch);
+      this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-        // Assemble all menu items
+      offswitch.connect("toggled", (object, value) => {
+        // We will just change the text content of the label
+        if (value) tinter.show();
+        else tinter.hide();
+      });
 
-        // This is a menu separator
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this.menu.addMenuItem(offswitch);
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+      this._redSlider = new Slider.Slider(0);
+      this._greenSlider = new Slider.Slider(0);
+      this._blueSlider = new Slider.Slider(0);
+      this._alphaSlider = new Slider.Slider(0);
 
-        offswitch.connect('toggled', (object, value) => {
-            // We will just change the text content of the label
-            if (value)
-                tinter.show();
-            else
-                tinter.hide();
-        });
+      let _redLabel = new St.Label({ text: "R" });
+      this._redSliderContainer = new PopupMenu.PopupBaseMenuItem({
+        activate: false,
+      });
+      this._redSliderContainer.add_child(_redLabel);
+      this._redSliderContainer.add_child(this._redSlider);
+      this.menu.addMenuItem(this._redSliderContainer);
 
+      let _greenLabel = new St.Label({ text: "G" });
+      this._greenSliderContainer = new PopupMenu.PopupBaseMenuItem({
+        activate: false,
+      });
+      this._greenSliderContainer.add_child(_greenLabel);
+      this._greenSliderContainer.add_child(this._greenSlider);
+      this.menu.addMenuItem(this._greenSliderContainer);
 
-        this._redSlider = new Slider.Slider(0);
-        this._greenSlider = new Slider.Slider(0);
-        this._blueSlider = new Slider.Slider(0);
-        this._alphaSlider = new Slider.Slider(0);
+      let _blueLabel = new St.Label({ text: "B" });
+      this._blueSliderContainer = new PopupMenu.PopupBaseMenuItem({
+        activate: false,
+      });
+      this._blueSliderContainer.add_child(_blueLabel);
+      this._blueSliderContainer.add_child(this._blueSlider);
+      this.menu.addMenuItem(this._blueSliderContainer);
 
+      this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-        let _redLabel = new St.Label({text: 'R'});
-        this._redSliderContainer = new PopupMenu.PopupBaseMenuItem({activate: false});
-        this._redSliderContainer.add_child(_redLabel);
-        this._redSliderContainer.add_child(this._redSlider);
-        this.menu.addMenuItem(this._redSliderContainer);
+      let _alphaLabel = new St.Label({ text: "Alpha" });
+      this._alphaSliderContainer = new PopupMenu.PopupBaseMenuItem({
+        activate: false,
+      });
+      this._alphaSliderContainer.add_child(_alphaLabel);
+      this._alphaSliderContainer.add_child(this._alphaSlider);
+      this.menu.addMenuItem(this._alphaSliderContainer);
 
+      this._redSlider.connect("notify::value", this._setColors.bind(this));
+      this._blueSlider.connect("notify::value", this._setColors.bind(this));
+      this._greenSlider.connect("notify::value", this._setColors.bind(this));
+      this._alphaSlider.connect("notify::value", this._setColors.bind(this));
 
-        let _greenLabel = new St.Label({text: 'G'});
-        this._greenSliderContainer = new PopupMenu.PopupBaseMenuItem({activate: false});
-        this._greenSliderContainer.add_child(_greenLabel);
-        this._greenSliderContainer.add_child(this._greenSlider);
-        this.menu.addMenuItem(this._greenSliderContainer);
-
-
-        let _blueLabel = new St.Label({text: 'B'});
-        this._blueSliderContainer = new PopupMenu.PopupBaseMenuItem({activate: false});
-        this._blueSliderContainer.add_child(_blueLabel);
-        this._blueSliderContainer.add_child(this._blueSlider);
-        this.menu.addMenuItem(this._blueSliderContainer);
-
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        let _alphaLabel = new St.Label({text: 'Alpha'});
-        this._alphaSliderContainer = new PopupMenu.PopupBaseMenuItem({activate: false});
-        this._alphaSliderContainer.add_child(_alphaLabel);
-        this._alphaSliderContainer.add_child(this._alphaSlider);
-        this.menu.addMenuItem(this._alphaSliderContainer);
-
-        this._redSlider.connect('notify::value', this._setColors.bind(this));
-        this._blueSlider.connect('notify::value', this._setColors.bind(this));
-        this._greenSlider.connect('notify::value', this._setColors.bind(this));
-        this._alphaSlider.connect('notify::value', this._setColors.bind(this));
-
-        this._getColors();
+      this._getColors();
     }
 
     _getColors() {
-        this._redSlider._setCurrentValue(this._redSlider, overlay_color['red'] / 255);
-        this._blueSlider._setCurrentValue(this._blueSlider, overlay_color['blue'] / 255);
-        this._greenSlider._setCurrentValue(this._greenSlider, overlay_color['green'] / 255);
-        this._alphaSlider._setCurrentValue(this._alphaSlider, overlay_color['alpha'] / 255);
+      this._redSlider._setCurrentValue(
+        this._redSlider,
+        overlay_color["red"] / 255
+      );
+      this._blueSlider._setCurrentValue(
+        this._blueSlider,
+        overlay_color["blue"] / 255
+      );
+      this._greenSlider._setCurrentValue(
+        this._greenSlider,
+        overlay_color["green"] / 255
+      );
+      this._alphaSlider._setCurrentValue(
+        this._alphaSlider,
+        overlay_color["alpha"] / 255
+      );
     }
 
     _setColors() {
-        overlay_color['red'] = 255 * this._redSlider._getCurrentValue();
-        overlay_color['green'] = 255 * this._greenSlider._getCurrentValue();
-        overlay_color['blue'] = 255 * this._blueSlider._getCurrentValue();
-        overlay_color['alpha'] = 255 * this._alphaSlider._getCurrentValue();
+      overlay_color["red"] = 255 * this._redSlider._getCurrentValue();
+      overlay_color["green"] = 255 * this._greenSlider._getCurrentValue();
+      overlay_color["blue"] = 255 * this._blueSlider._getCurrentValue();
+      overlay_color["alpha"] = 255 * this._alphaSlider._getCurrentValue();
 
-        tinter.setOverlayColor();
+      tinter.setOverlayColor();
     }
-});
-
-
+  }
+);
 
 /**
  *
  */
 function enable() {
-    tinter = new ColorTinter();
-    tinter.start_up();
-    menu = new MenuButton();
-    Main.panel.addToStatusArea('Tint', menu, 0, 'right');
+  tinter = new ColorTinter();
+  tinter.start_up();
+  menu = new MenuButton();
+  Main.panel.addToStatusArea("Tint", menu, 0, "right");
 }
 
 /**
  *
  */
 function disable() {
-    tinter.stop_now();
-    tinter = null;
-    menu.destroy();
-    menu = null;
+  tinter.stop_now();
+  tinter = null;
+  menu.destroy();
+  menu = null;
 }
-
 
 /**
  *
  */
-function init() {
-
-
-}
-
+function init() {}
