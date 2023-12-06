552c1cc24be96c6b9c3bad0f5b1d79bb33eaee46
Matt Barnard <contact@missingbit.co.uk>
2023-12-06 15:57:25 +0000

    new class structure

--- src/extension.js
+++ src/extension.js
@@ -7,7 +7,6 @@ import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import * as Slider from "resource:///org/gnome/shell/ui/slider.js";
 
-import * as ExtensionUtils from "resource:///org/gnome/shell/misc/extensionUtils.js";
 const Me = ExtensionUtils.getCurrentExtension();
 let overlay_active = false;
 let tinter = null;
@@ -23,108 +22,150 @@ let overlay_color = {
 let settings = null;
 let ExtensionPath;
 ExtensionPath = Me.path;
-const ColorTinter = GObject.registerClass(
-  {
-    GTypeName: "ColorTinter",
-  },
-  class ColorTinter extends GObject.Object {
-    // Create Tint Overlay
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+export default class ColorTinter extends Extension {
+  /**
+   * This class is constructed once when your extension is loaded, not
+   * enabled. This is a good time to setup translations or anything else you
+   * only do once.
+   *
+   * You MUST NOT make any changes to GNOME Shell, connect any signals or add
+   * any event sources here.
+   *
+   * @param {ExtensionMeta} metadata - An extension meta object
+   */
+  constructor(metadata) {
+    super(metadata);
 
-    createOverlay() {
-      /*
+    console.debug(`constructing ${this.metadata.name}`);
+  }
+
+  /**
+   * This function is called when your extension is enabled, which could be
+   * done in GNOME Extensions, when you log in or when the screen is unlocked.
+   *
+   * This is when you should setup any UI for your extension, change existing
+   * widgets, connect signals or modify GNOME Shell's behavior.
+   */
+  enable() {
+    console.debug(`enabling ${this.metadata.name}`);
+    this.start_up();
+    menu = new MenuButton();
+    Main.panel.addToStatusArea("Tint", menu, 0, "right");
+    if (settings === null) {
+      settings = ExtensionUtils.getSettings(
+        "org.gnome.shell.extensions.colortint"
+      );
+    }
+  }
+
+  /**
+   * This function is called when your extension is uninstalled, disabled in
+   * GNOME Extensions or when the screen locks.
+   *
+   * Anything you created, modified or setup in enable() MUST be undone here.
+   * Not doing so is the most common reason extensions are rejected in review!
+   */
+  disable() {
+    console.debug(`disabling ${this.metadata.name}`);
+    this.stop_now();
+    menu.destroy();
+    menu = null;
+    settings = null;
+  }
+
+  createOverlay() {
+    /*
         Set the overlay to 100x the primary monitor's width and height. Set the overlay x and y to 0.
         This is hacky, but should cover most multi-setups.
         What should be done, is to iterate over all monitors, and create a seperate overlay for each that fills each
         according to its dimensions. If anyone wants to refactor this in that way, please do.
          */
 
-      let monitor = Main.layoutManager.primaryMonitor;
-      overlay = new St.Bin({ reactive: false });
-      overlay.set_size(monitor.width * 100, monitor.height * 100);
-      overlay.opacity = 255;
-      overlay.set_position(0, 0);
-      // Arbitrary z position above everything else
-      overlay.set_z_position(650);
+    let monitor = Main.layoutManager.primaryMonitor;
+    overlay = new St.Bin({ reactive: false });
+    overlay.set_size(monitor.width * 100, monitor.height * 100);
+    overlay.opacity = 255;
+    overlay.set_position(0, 0);
+    // Arbitrary z position above everything else
+    overlay.set_z_position(650);
 
-      this.setOverlayColor();
-    }
+    this.setOverlayColor();
+  }
 
-    // Update color of Overlay
-    setOverlayColor() {
-      var color = new Clutter.Color({
-        red: overlay_color["red"],
-        green: overlay_color["green"],
-        blue: overlay_color["blue"],
-        alpha: overlay_color["alpha"],
-      });
-      overlay.set_background_color(color);
-      this.saveColor();
-    }
+  // Update color of Overlay
+  setOverlayColor() {
+    var color = new Clutter.Color({
+      red: overlay_color["red"],
+      green: overlay_color["green"],
+      blue: overlay_color["blue"],
+      alpha: overlay_color["alpha"],
+    });
+    overlay.set_background_color(color);
+    this.saveColor();
+  }
 
-    // Hide Overlay
-    hide() {
-      overlay_active = false;
-      Main.uiGroup.remove_actor(overlay);
-    }
+  // Hide Overlay
+  hide() {
+    overlay_active = false;
+    Main.uiGroup.remove_actor(overlay);
+  }
 
-    // Show Overlay
-    show() {
-      Main.uiGroup.add_actor(overlay);
-      overlay_active = true;
-    }
+  // Show Overlay
+  show() {
+    Main.uiGroup.add_actor(overlay);
+    overlay_active = true;
+  }
 
-    // Load Color
-    loadColor() {
-      // Load last from json
+  // Load Color
+  loadColor() {
+    // Load last from json
 
-      this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
-      if (this._file.query_exists(null)) {
-        var flag;
-        var data;
-        [flag, data] = this._file.load_contents(null);
+    this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
+    if (this._file.query_exists(null)) {
+      var flag;
+      var data;
+      [flag, data] = this._file.load_contents(null);
 
-        if (flag) {
-          const ByteArray = imports.byteArray;
-          let prepData =
-            data instanceof Uint8Array
-              ? ByteArray.toString(data)
-              : data.toString();
-          overlay_color = JSON.parse(prepData);
-        }
+      if (flag) {
+        const ByteArray = imports.byteArray;
+        let prepData =
+          data instanceof Uint8Array
+            ? ByteArray.toString(data)
+            : data.toString();
+        overlay_color = JSON.parse(prepData);
       }
-    }
-
-    // Save Color
-    saveColor() {
-      this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
-      this._file.replace_contents(
-        JSON.stringify(overlay_color),
-        null,
-        false,
-        0,
-        null
-      );
-    }
-
-    // enable
-    start_up() {
-      overlay_active = false;
-      this.loadColor();
-      this.createOverlay();
-      if (settings instanceof Gio.Settings) {
-        if (settings.get_boolean("autostart")) this.show();
-      }
-    }
-
-    // disable
-    stop_now() {
-      if (overlay_active == true) Main.uiGroup.remove_actor(overlay);
-
-      overlay.destroy();
-      overlay = null;
     }
   }
-);
+
+  // Save Color
+  saveColor() {
+    this._file = Gio.file_new_for_path(`${ExtensionPath}/settings.json`);
+    this._file.replace_contents(
+      JSON.stringify(overlay_color),
+      null,
+      false,
+      0,
+      null
+    );
+  }
+
+  start_up() {
+    overlay_active = false;
+    this.loadColor();
+    this.createOverlay();
+    if (settings instanceof Gio.Settings) {
+      if (settings.get_boolean("autostart")) this.show();
+    }
+  }
+
+  stop_now() {
+    if (overlay_active == true) Main.uiGroup.remove_actor(overlay);
+
+    overlay.destroy();
+    overlay = null;
+  }
+}
 
 const MenuButton = GObject.registerClass(
   { GTypeName: "MenuButton" },
@@ -261,34 +302,3 @@ const MenuButton = GObject.registerClass(
     }
   }
 );
-
-/**
- *
- */
-function enable() {
-  tinter = new ColorTinter();
-  tinter.start_up();
-  menu = new MenuButton();
-  Main.panel.addToStatusArea("Tint", menu, 0, "right");
-  if (settings === null) {
-    settings = ExtensionUtils.getSettings(
-      "org.gnome.shell.extensions.colortint"
-    );
-  }
-}
-
-/**
- *
- */
-function disable() {
-  tinter.stop_now();
-  tinter = null;
-  menu.destroy();
-  menu = null;
-  settings = null;
-}
-
-/**
- *
- */
-function init() {}
